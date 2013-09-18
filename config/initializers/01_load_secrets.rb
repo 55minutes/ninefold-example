c23cd5188a3252ce6e946c5044e2590e7f352124
Matt Brictson <matt@55minutes.com>
2013-09-18 13:20:33 -0700

    Fail gracefully if secrets.yml doesn't exist

--- config/initializers/01_load_secrets.rb
+++ config/initializers/01_load_secrets.rb
@@ -1,2 +1,8 @@
-raw_config = File.read("#{Rails.root}/config/secrets.yml")
-SECRETS = YAML.load(raw_config)[Rails.env].symbolize_keys
+secrets_file = "#{Rails.root}/config/secrets.yml"
+
+SECRETS = if File.exists?(secrets_file)
+  raw_config = File.read(secrets_file)
+  YAML.load(raw_config)[Rails.env].symbolize_keys
+else
+  Hash.new
+end
